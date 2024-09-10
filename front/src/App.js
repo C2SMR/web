f5c2cc0fa477db4f0bdd455f44333b82d74b9f13
victordalet <victor.dalet@protonmail.com>
2024-09-10 20:14:30 +0200

    feat(front): Add view pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Privacy } from "./pages/privacy";
-import { Zone } from "./pages/zone";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Home} from "./pages/home";
+import {Privacy} from "./pages/privacy";
+import {Zone} from "./pages/zone";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-          <Route path={"/privacy"} element={<Privacy/>}></Route>
-            <Route path={"/zone"} element={<Zone/>}></Route>
-          <Route path={"*"} element={<Home/>}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/privacy"} element={<Privacy/>}></Route>
+                <Route path={"/zone"} element={<Zone/>}></Route>
+                <Route path={"/view"} element={<Home/>}></Route>
+                <Route path={"*"} element={<Home/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
